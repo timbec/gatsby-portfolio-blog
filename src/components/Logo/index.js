8102d0481546a8026981bf221ad5d15942115cbb
Tim Beckett <timbeckett@ymail.com>
2021-12-02 15:54:22 -0500

    tentative first attempt at logo/design elements.

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -11,27 +11,28 @@ const Logo = () => {
     return (
         <Fragment>
             <span className="site-header__logos--title">
+            
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                    height="300" width="300"
-                    class="triangle">
+                    height="300" width="400"
+                    className="triangle">
                     <title>Triangle</title>
-                        <polygon points="300,60 100,300 300,300" 
+                        <polygon points="250,60 100,400 400,400" 
                         style={{fill: '#c6372c', stroke:'#c6372c', fillOpacity:1,strokeOpacity:0.9 }} />
                         Sorry, your browser does not support inline SVG. 
                 </svg>
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                    width="200" height="200"
+                    width="150" height="150"
                     class="black-square">
                     <title>Square</title>
-                    <rect x="50" y="20" width="150" height="150"
+                    <rect x="30" y="20" width="120" height="120"
                     style={{fill:'#09012d', stroke:'#09012d', fillOpacity:1, strokeOpacity:0.9 }} />
                     Sorry, your browser does not support inline SVG. 
                 </svg>
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                    width="200" height="200"
+                    width="150" height="150"
                     class="red-square">
                     <title>Square</title>
-                    <rect x="50" y="20" width="150" height="150"
+                    <rect x="20" y="20" width="120" height="120"
                     style={{fill:'#c6372c', stroke:'#c6372c', fillOpacity:1, strokeOpacity:0.9 }} />
                     Sorry, your browser does not support inline SVG. 
                 </svg>
