2853b2b5c729b9415173e96a9c70c87ea193e1ae
Tim Beckett <timbeckett@Tims-Mac-mini.home>
2022-02-11 16:20:42 -0500

    style updates, blog post update.

---
+++ Notes/multiple-columns.md
@@ -0,0 +1,8 @@
+https://www.w3schools.com/css/css3_multiple_columns.asp
+
+```
+.element {
+    column-width: 30%; 
+    column-count: 3; 
+    column-gap: 5rem; 
+}
--- src/components/Blog/BlogIntro.js
+++ src/components/Blog/BlogIntro.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import { Fragment } from "react"
 import { Link } from "gatsby"
 
@@ -6,23 +6,23 @@ import { Link } from "gatsby"
 const BlogIntro = () => {
     return (
         <article className="blog__intro">
-        <h2 className="blog__intro--title">Welcome to my Blog</h2>
-        <p>
-        This is part of an ongoing series based on the principle of 'learning in public'. I don't plan on writing anything particularly groundbreaking in the beginning, I just want to get in the habit of what I do for a living, what I'm learning, and what I find interesting. I plan on writing about, in no particular order, web development (in particular front end development), Blockchain technologies, including NFTs, the Metaverse, Machine Learning and AI generally. 
-        </p>
+            <h2 className="blog__intro--title">Welcome to my Blog</h2>
+            <div className="blog__intro--content">
+                <p>
+                    This is part of an ongoing series based on the principle of 'learning in public'.
 
-        <p>As I gain more knowledge (and frankly confidence), I plan to write about books and articles I'm reading, perhaps providing reviews, and tech trends I find interesting (or troubling - we certainly have no shortage of that). </p>
+                    I don't plan on writing anything groundbreaking for now, I just want to get in the habit of writing about what I do for a living, what I'm learning, and things I find interesting. I write about, in no particular order, web development, Blockchain technologies, including NFTs, the Metaverse, Machine Learning and AI generally.
+                </p>
 
-        <p>For the moment, I plan on just throwing up articles as I write them, but soon I will add categories and tags, creating separate pages for each category. Then . . . commenting.</p>
+                <p>As I gain more knowledge (and frankly confidence), I plan to write about books and articles I'm reading, perhaps providing reviews, as well as tech trends I find interesting (or troubling - we certainly have no shortage of that). </p>
 
-        <p>I used to blog quite a bit, as was the trend in the late '00s and early '10s, when social media still allowed for things like small-fry bloggers such as me. When social media was still seen as a force for good. Oh, how naive we were. One of these blog still exists at: https://livefromtheheygate.blogspot.com/. For the other, I will eventually put up selected posts on my writing site at: https: timbeckett-writing.com. </p>
+                <p>For the moment, I plan on putting up articles as I write them, but soon I will add categories and tags, creating separate pages for each category. Then . . . commenting.</p>
 
-        <p>
-        <em>This is part of an ongoing series based on the principle of 'learning in public'. </em>
-        </p>
-            
-        
-        </article>
+                <p>I used to blog quite a bit, as was the trend in the late '00s and early '10s, when social media still allowed for such things like small-fry bloggers such as me. When social media was still seen as a force for good (Oh, how naive we were). One of these blog still exists at: https://livefromtheheygate.blogspot.com/. For the other, I will eventually put up selected posts on my writing site at: https: timbeckett-writing.com. </p>
+            </div>
+
+
+        </article >
     )
 }
 
