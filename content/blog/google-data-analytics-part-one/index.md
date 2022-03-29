0efc2b424aa6c376717eee4e0e8e442ba0c8ce5f
Tim Beckett <timbeckett@Tims-Air.home>
2022-03-29 12:48:44 -0400

    style updates to the front page.

---
+++ content/blog/google-data-analytics-part-one/index.md
@@ -0,0 +1,38 @@
+---
+title: Google Data Anlysis Course - Part One - Data Data Everywhere
+date: "2022-03-16"
+description: "Section One of the Google Analysis Course: Data Data Everywhere"
+featuredImage: istockphoto-data-analysis-612x612.jpg
+slug: "blog/google-data-analytics-section-one"
+---
+
+> A Data Analyst is an explorer, an artist, and a detective all rolle into one.
+
+Section One (Data, Data Everyhere) of the Google Data Analytics Course lays out the introduction to the course to come. It effective lays out the basics of data analysis, how to understand data, and what steps we will be using as we attempt to become proficient data analysts.
+
+We started with the basics:
+
+- The **data ecosystem** is the elements that interact with one another to produce manage, store, organize and analyze data.
+- **data science**: the creation of new ways ot modelling and understanding the unknown by using raw data.
+- **data analysis**: The collection, tranformation and organization of data in order to draw conclusions, make predictions and drive informed decision-making.
+- **data analytics**: the science of data.
+
+Very early on, the instructors brought up the concept of **data-drive decision-making** which will occur as a key concept throughout the course.
+
+The key skills for a data analyst are:
+
+- cursiosity
+- an understanding of context
+- the ability to adopt a technical mindset
+- an awareness of data design
+- an ability to plan data strategy.
+
+**Curiousity** is the most obvious - and the most important. A data analyst has to want to _Understand_ things, to seek out stories and patterns.
+
+**Understanding context** no analysis of raw data is useful without understanding the context of how it was collected, the factors that influenced the data collected. Context is everything in understanding data.
+
+**technical mindset** is the ability to break down a task into smaller steps, using an orderly process to do so.
+
+**data design** is you we can organize data. How will be sort our data, for example alphabetical or numerical order, size, or other means.
+
+**data strategy** is the management of the people, processes and tools we will be using in our analysis, and the steps we will be following to work through the process.
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import Image from "../components/image"
 
-
 const IndexPage = () => {
   return (
     <Layout pageName="home">
@@ -13,86 +12,102 @@ const IndexPage = () => {
         <title>Home Page</title>
       </Helmet>
 
-        <section id="intro" className="intro">
-          <article className="intro__text">
-            <h2>Welcome</h2>
+      <section id="intro" className="intro">
+        <article className="intro__text">
+          <h2>Welcome</h2>
 
-            <p>
-              I am a Full-Stack Developer specializing in Javascript, PHP and
-              Python. I have used Wordpress, Laravel, Django and, most recently, Gatsby.js.
-              This portfolio site (with attached blog) is my first site built in Gatsby.
-            </p>
+          <p>
+            I am a Full-Stack Developer specializing in Javascript, PHP and
+            Python. I have used Wordpress, Laravel, Django and, most recently,
+            Gatsby.js. This portfolio site (with attached blog) is my first site
+            built in Gatsby.
+          </p>
 
-            <p>
-              I have been lucky to have some great clients from diverse fields,
-              including the NYTimes, EPIX TV, Weil Gotshal and Manges, Carnegie,
-              and Lazard Asset Management among others. For a full (er) list,
-              see below or visit my [LinkedIn profile][https://linkedin.com/in/timbeckett1]
-            </p>
+          <p>
+            I have been lucky to have some great clients from diverse fields,
+            including the NYTimes, EPIX TV, Weil Gotshal and Manges, Carnegie,
+            and Lazard Asset Management, among others. For a full (er) list, see
+            below or visit my
+            <a href="https://linkedin.com/in/timbeckett1">LinkedIn Profile</a>
+          </p>
 
-            <p>
-              As well as web development, I have become interested in the so-called Web 3.0 - Data Science, Machine Learning, AR/VR, the Blockchain. To that end I have been studying the basics of data science - Data Analysis, Machine Learning, Data Visualization, and the Solidity language used for smart contracts in the Blockchain, as well as learning about the blockchain generally. 
-            </p>
+          <p>
+            I have recently become interested in the so-called Web 3.0 - Data
+            Science, Machine Learning, AR/VR, the Blockchain. To that end I have
+            been studying the basics of data science - Data Analysis, Machine
+            Learning, Data Visualization, and the Solidity language used for
+            smart contracts in the Blockchain, as well as learning about the
+            blockchain generally.
+          </p>
 
-            <p>
-              I came to software development from a background in media. With this site, I intend to start writing about Web 3.0, and various obstacles and developments I've encountered as a web developer. 
-            </p>
+          <p>
+            Currently working through the
+            <a href="https://www.coursera.org/professional-certificates/google-data-analytics">
+              Google Data Analytics Certificate Course
+            </a>
+          </p>
 
-            <p>This site remains under construction . . . </p>
-          </article>
-          <article className="intro__collage">
-            <ul className="intro__collage--list">
-              <li className="intro__collage--photo">
-                <Image src={`collage-images/Writing.jpg`} />
-              </li>
-              <li className="intro__collage--photo">
-                <Image src={`collage-images/opera-slider.jpg`} />
-              </li>
-              <li className="intro__collage--photo">
-                <Image src={`collage-images/Unbearables_2.jpg`} />
-              </li>
-            </ul>
-          </article>
-        </section>
-        <section id="portfolio" className="portfolio">
-          <h2 className="portfolio__heading">Portfolio</h2>
-          <ul className="portfolio__images">
-            <li className="portfolio__images--image one">
-              <Image src={`client-logos/new-epix-logo.png`} />
-            </li>
-            <li className="portfolio__images--image large-logo two">
-              <Image src={`client-logos/nytimes_logo.jpg`} />
-            </li>
-            <li className="portfolio__images--image large-logo three">
-              <Image src={`client-logos/millenium-full.jpg`} />
-            </li>
-            <li className="portfolio__images--image square-logo four">
-              <Image src={`client-logos/sullivan.jpg`} />
-            </li>
+          <p>
+            I came to software development from a background in media.
+            Tentatively, I've started writing about
+            <a href="/blog"> things I'm working on/interested in.</a>
+          </p>
 
-            <li className="portfolio__images--image small-logo five">
-              <Image src={`client-logos/greatimmigrants.jpg`} />
+          <p>This site remains under construction . . . </p>
+        </article>
+        <article className="intro__collage">
+          <ul className="intro__collage--list">
+            <li className="intro__collage--photo">
+              <Image src={`collage-images/Writing.jpg`} />
             </li>
-            <li className="portfolio__images--image square-logo six">
-              <Image src={`client-logos/opera-rect.png`} />
+            <li className="intro__collage--photo">
+              <Image src={`collage-images/opera-slider.jpg`} />
             </li>
-            <li className="portfolio__images--image med-logo seven">
-              <Image src={`client-logos/lazard_asset_management_logo.jpg`} />
-            </li>
-            <li className="portfolio__images--image thin-logo eight">
-              <Image src={`client-logos/weil.png`} />
-            </li>
-            <li className="portfolio__images--image small-logo nine">
-              <Image src={`client-logos/wsj.jpg`} />
-            </li>
-            <li className="portfolio__images--image small-logo ten">
-              <Image src={`client-logos/nyu-law.png`} />
-            </li>
-            <li className="portfolio__images--image small-logo eleven">
-              <Image src={`client-logos/Digitas_Health_Logo.jpg`} />
+            <li className="intro__collage--photo">
+              <Image src={`collage-images/Unbearables_2.jpg`} />
             </li>
           </ul>
-        </section>
+        </article>
+      </section>
+      <section id="portfolio" className="portfolio">
+        <h2 className="portfolio__heading">Portfolio</h2>
+        <ul className="portfolio__images">
+          <li className="portfolio__images--image one">
+            <Image src={`client-logos/new-epix-logo.png`} />
+          </li>
+          <li className="portfolio__images--image large-logo two">
+            <Image src={`client-logos/nytimes_logo.jpg`} />
+          </li>
+          <li className="portfolio__images--image large-logo three">
+            <Image src={`client-logos/millenium-full.jpg`} />
+          </li>
+          <li className="portfolio__images--image square-logo four">
+            <Image src={`client-logos/sullivan.jpg`} />
+          </li>
+
+          <li className="portfolio__images--image small-logo five">
+            <Image src={`client-logos/greatimmigrants.jpg`} />
+          </li>
+          <li className="portfolio__images--image square-logo six">
+            <Image src={`client-logos/opera-rect.png`} />
+          </li>
+          <li className="portfolio__images--image med-logo seven">
+            <Image src={`client-logos/lazard_asset_management_logo.jpg`} />
+          </li>
+          <li className="portfolio__images--image thin-logo eight">
+            <Image src={`client-logos/weil.png`} />
+          </li>
+          <li className="portfolio__images--image small-logo nine">
+            <Image src={`client-logos/wsj.jpg`} />
+          </li>
+          <li className="portfolio__images--image small-logo ten">
+            <Image src={`client-logos/nyu-law.png`} />
+          </li>
+          <li className="portfolio__images--image small-logo eleven">
+            <Image src={`client-logos/Digitas_Health_Logo.jpg`} />
+          </li>
+        </ul>
+      </section>
     </Layout>
   )
 }
