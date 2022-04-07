ae23540b54b604556440b49644f98d45f6558b47
Tim Beckett <timbeckett@Tims-Air.home>
2022-04-07 16:49:12 -0400

    updated to 'about' page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
-import React from 'react'; 
-import { Link, graphql } from "gatsby"; 
+import React from "react"
+import { Link, graphql } from "gatsby"
 
 import { Helmet } from "react-helmet"
 
@@ -7,10 +7,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 
 const AboutPage = () => {
-
-    return (
-        <Layout>
-        <section id="about" className="page">
+  return (
+    <Layout>
+      <section id="about" className="page">
         <article className="article">
           <h2 className="article--heading">About Me</h2>
 
@@ -19,27 +18,28 @@ const AboutPage = () => {
               <Image src={`about/self-portrait.jpg`} />
             </div>
             <div className="article--body-text">
-            <p>
-              I am a full-stack developer though I tilt more toward the
-              front-end. I'm proficient in Javascript, Python, and PHP. I've used Wordpress, Laravel, ResourceSpace, Meteor, Node,
-              D3, and many other frameworks. Out of necessity I've developed
-              some proficiency in AWS, though by no means an expert.
-            </p>
-            <p>
-              I blog about (for now) random elements of front end development, Data
-              Visualization, Data Analysis generally, and AI and ML, and Blockchain technology, all of
-              which reflect my current interests in tech and the world in which we live.
-              in.
-            </p>
+              <p>
+                I am a full-stack developer though I tilt more toward the
+                front-end. I'm proficient in Javascript, Python, and PHP. I've
+                used Wordpress, Laravel, ResourceSpace, Meteor, Node, D3, and
+                many other frameworks. Out of necessity I've developed some
+                proficiency in AWS, though by no means an expert.
+              </p>
+              <p>
+                I blog about (for now) random elements of front end development,
+                Data Visualization, Data Analysis generally, and AI and ML, and
+                Blockchain technology, all of which reflect my current interests
+                in tech and the world in which we live.
+              </p>
             </div>
           </div>
         </article>
         <figure className="article--image">
-            <Image src=""></Image>
+          <Image src=""></Image>
         </figure>
       </section>
-      </Layout>
-    )
+    </Layout>
+  )
 }
 
-export default AboutPage; 
+export default AboutPage
